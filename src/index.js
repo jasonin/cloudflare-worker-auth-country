3566baac6b96b9d6b874e2f0b71f025892004815
Jason In <jasonin@hotmail.com>
2023-10-18 17:28:33 +0800

    Handle JWT Token and HTTP Response

--- src/index.js
+++ src/index.js
@@ -10,6 +10,32 @@
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const jwt = request.headers.get('cf-access-jwt-assertion');
+		if (!jwt) {
+			return new Response('Unauthorized', { status: 401 });
+		}
+
+		// Split the JWT into its parts
+		const jwtParts = jwt.split('.');
+		const headerBase64Url = jwtParts[0];
+		const payloadBase64Url = jwtParts[1];
+
+		// Base64Url decode the payload
+		const payloadBase64 = payloadBase64Url.replace('-', '+').replace('_', '/');
+		const payloadString = atob(payloadBase64);
+		const payload = JSON.parse(payloadString);
+		
+		const email = payload.email;
+  	const country = payload.country;
+
+		const timestamp = payload.iat || payload.auth_time;
+		const date = new Date(timestamp * 1000).toISOString(); // Convert from seconds to milliseconds
+	
+		// Crafting the response
+		let responseHtml = `<p>${email} authenticated at ${date} from <a href="/secure/${country}">${country}</a></p>`;
+
+		return new Response(responseHtml, {
+			headers: { 'Content-Type': 'text/html' }
+		});	
 	},
 };
